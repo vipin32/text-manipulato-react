45be0b2f261a6ed51428a64452896a8e624219c2
Vipin32 <36256756+Vipin32@users.noreply.github.com>
2022-04-10 15:01:33 +0530

    Display total words and characters

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,22 +2,24 @@ import React, {useState} from 'react';
 
 export default function Form(props) {
 
-    const handleUpClick = () => {
+    const handleUpperClick = () => {
 
         let newText = userText.toUpperCase();
-
         setUserText(newText);
-        // console.log(newText);
-
-
     }
+
+    const handleLowerClick = () => {
+        let newText = userText.toLowerCase();
+        setUserText(newText);
+    }
+
+    
 
     const handleTextOnChange = (event) => {
         setUserText(event.target.value);
-        // console.log(userText);
     }
 
-    const [userText, setUserText] = useState('Enter Any Text');
+    const [userText, setUserText] = useState('');
 
 
   return (
@@ -28,7 +30,17 @@ export default function Form(props) {
             <textarea name="userText" id="userText" value={userText} onChange={handleTextOnChange} cols="30" rows="10" className="form-control"></textarea>
         </div>
 
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button>
+        <button className="btn btn-primary" onClick={handleUpperClick}>Convert To Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLowerClick}>Convert To Uppercase</button>
+      </div>
+
+      <div className="container my-2">
+          <h2>Text Summary</h2>
+          <p>{userText.split(' ').length} words and {userText.length} character</p>
+          <p>{0.08 * userText.split(' ').length} minutes read</p>
+
+          <h3>Preview</h3>
+          <p>{userText}</p>
       </div>
 
       </>
